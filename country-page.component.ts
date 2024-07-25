98d798d92c06c672667010ef29bf57ccf0c163f2
Deva-354 <130522847+Deva-354@users.noreply.github.com>
2024-07-25 12:24:28 -0400

    Update country-page.component.ts

--- country-page.component.ts
+++ country-page.component.ts
@@ -1,14 +1,18 @@
 import { Component,OnInit } from '@angular/core';
 import { DataService } from '../../../shared/services/data.service';
+import { SearchService } from '../../../shared/services/search.service';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrl: './country-page.component.css'
-})
+}) 
 export class CountryPageComponent  implements OnInit{
 covidData: any;
-  constructor(private dataService: DataService){}
+  constructor(
+    private dataService: DataService
+    private searchService: SearchService
+  ){}
   ngOnInit(): void {
     this.dataService.getSummaryData()
     .subscribe(
@@ -24,7 +28,7 @@ covidData: any;
       (c:any) => c.Country.toLowerCase().includes(country.toLowerCase())
     );
 
-    console.log('Countries',Countries);
+    this.searchService.setCountries();
  }
 
 }
